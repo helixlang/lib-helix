124c95154070aef2bbaf4ea1297ef64d734aa5eb
Dhruvan <dhruvan.k05@gmail.com>
2024-12-10 02:43:47 -0500

    refactor: reorganize includes in libc.h and enhance type definitions in types.h for better platform compatibility

--- core/include/types.h
+++ core/include/types.h
@@ -16,8 +16,6 @@
 #ifndef __$LIBHELIX_TYPES__
 #define __$LIBHELIX_TYPES__
 
-#include <cstddef>
-
 using u8  = unsigned char;
 using i8  = signed char;
 using u16 = unsigned short;
@@ -27,16 +25,39 @@ using i32 = signed int;
 using u64 = unsigned long long;
 using i64 = signed long long;
 
-#if !defined(_MSC_VER)
-using u128 = __uint128_t;
-using i128 = __int128_t;
-#endif
+// i128 and u128 are derived data types and might be simulated using a struct
 
 using f32 = float;
 using f64 = double;
 using f80 = long double;
 
-using usize = std::size_t;
-using isize = std::ptrdiff_t;
+#if defined(__LP64__)     || defined(_WIN64)     || defined(__x86_64__) || defined(__ppc64__) || \
+    defined(__aarch64__)  || defined(__arm64__)  || defined(__mips64__) || defined(__mips64)  || \
+    defined(__mips64el__) || defined(__mips64el) || defined(__s390x__)
+using usize = unsigned long long;
+using isize = long long;
 
+#elif defined(__ILP32__) || defined(_WIN32) || defined(__i386__)   || defined(__arm__)  || \
+    defined(__mips__)    || defined(__mips) || defined(__mipsel__) || defined(__mipsel) || \
+    defined(__s390__)
+using usize = unsigned int;
+using isize = int;
+
+#elif defined(__16BIT__) || defined(__MSP430__) || defined(__AVR__)
+using usize = unsigned short;
+using isize = short;
+
+#elif defined(__8BIT__)
+using usize = unsigned char;
+using isize = char;
+
+#else
+#error \
+    "Helix core: Unable to determine platform bitness. Supported macros: __LP64__, __ILP32__, __16BIT__, __8BIT__. Define one explicitly using '-D' during compilation."
 #endif
+
+// ensure that the sizes are correct and match the platform
+static_assert(sizeof(usize) == sizeof(void *), "usize must match the size of a pointer.");
+static_assert(sizeof(isize) == sizeof(void *), "isize must match the size of a pointer.");
+
+#endif  // __$LIBHELIX_TYPES__
