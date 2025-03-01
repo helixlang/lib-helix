3ee4ade0e8ae8877d62ee94a9ef24db0e686e29f
Dhruvan <dhruvan.k05@gmail.com>
2025-03-01 01:03:17 -0500

    feat(core): Add platform-specific header files
    This commit adds platform-specific header files for Darwin, libc++, Posix, and Windows in the core/include/c++ directory. These header files are necessary for the Helix Project and are part of the Attribution 4.0 International license (CC BY 4.0). The files include necessary system headers for each platform and are organized into separate files for better code organization and maintainability.
    
    The changes introduce new files:
    - darwin.hh
    - libc++.hh
    - posix.hh
    - windows.hh
    
    These header files will be used to enable platform-specific functionality in the Helix Project.

---
+++ core/include/c++/libc++.hh
@@ -0,0 +1,79 @@
+///--- The Helix Project ------------------------------------------------------------------------///
+///                                                                                              ///
+///   Part of the Helix Project, under the Attribution 4.0 International license (CC BY 4.0).    ///
+///   You are allowed to use, modify, redistribute, and create derivative works, even for        ///
+///   commercial purposes, provided that you give appropriate credit, and indicate if changes    ///
+///   were made.                                                                                 ///
+///                                                                                              ///
+///   For more information on the license terms and requirements, please visit:                  ///
+///     https://creativecommons.org/licenses/by/4.0/                                             ///
+///                                                                                              ///
+///   SPDX-License-Identifier: CC-BY-4.0                                                         ///
+///   Copyright (c) 2024 The Helix Project (CC BY 4.0)                                           ///
+///                                                                                              ///
+///-------------------------------------------------------------------------------- Lib-Helix ---///
+
+#ifndef _$_HX_CORE_M6LIBCPP
+#define _$_HX_CORE_M6LIBCPP
+
+#include "../config/config.h"
+
+#include "windows.hh"
+#include "darwin.hh"
+#include "posix.hh"
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <chrono>
+#include <complex>
+#include <condition_variable>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <optional>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+#include <shared_mutex>
+#include <span>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <variant>
+#include <vector>
+
+#endif  // _$_HX_CORE_M6LIBCPP
---
+++ core/include/c/libc.hh
@@ -0,0 +1,110 @@
+///--- The Helix Project ------------------------------------------------------------------------///
+///                                                                                              ///
+///   Part of the Helix Project, under the Attribution 4.0 International license (CC BY 4.0).    ///
+///   You are allowed to use, modify, redistribute, and create derivative works, even for        ///
+///   commercial purposes, provided that you give appropriate credit, and indicate if changes    ///
+///   were made.                                                                                 ///
+///                                                                                              ///
+///   For more information on the license terms and requirements, please visit:                  ///
+///     https://creativecommons.org/licenses/by/4.0/                                             ///
+///                                                                                              ///
+///   SPDX-License-Identifier: CC-BY-4.0                                                         ///
+///   Copyright (c) 2024 The Helix Project (CC BY 4.0)                                           ///
+///                                                                                              ///
+///-------------------------------------------------------------------------------- Lib-Helix ---///
+
+#ifndef _$_HX_CORE_M4LIBC
+#define _$_HX_CORE_M4LIBC
+
+#undef _LIBCPP_BEGIN_NAMESPACE_STD
+#undef _LIBCPP_END_NAMESPACE_STD
+#undef _VSTD
+#  define _LIBCPP_BEGIN_NAMESPACE_STD namespace _LIBCPP_TYPE_VISIBILITY_DEFAULT libc { inline namespace _LIBCPP_ABI_NAMESPACE {
+#  define _LIBCPP_END_NAMESPACE_STD }}
+#  define _VSTD libc
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio>
+#include <cassert>
+#include <ccomplex>
+#include <csignal>
+#include <ctgmath>
+#include <cmath>
+#include <cerrno>
+#include <cfenv>
+#include <clocale>
+#include <ctime>
+#include <csetjmp>
+#include <cstdarg>
+#include <cuchar>
+#include <cwchar>
+#include <cstring>
+#include <cwctype>
+#include <cctype>
+#include <cinttypes>
+#include <cfloat>
+
+#undef _LIBCPP_BEGIN_NAMESPACE_STD
+#undef _LIBCPP_END_NAMESPACE_STD
+#undef _VSTD
+#  define _LIBCPP_BEGIN_NAMESPACE_STD namespace _LIBCPP_TYPE_VISIBILITY_DEFAULT std { inline namespace _LIBCPP_ABI_NAMESPACE {
+#  define _LIBCPP_END_NAMESPACE_STD }}
+#  define _VSTD std
+
+namespace libc {
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/timeb.h>
+#include <sys/types.h>
+#include <memory.h>
+#include <search.h>
+#include <stdnoreturn.h>
+
+#ifndef _MSC_VER
+    #include "cxxabi.h"
+    #include <unistd.h>
+#else
+    #include <sys/utime.h>
+    #include <sys/locking.h>
+    #include <conio.h>
+    #include <corecrt.h>
+    #include <corecrt_io.h>
+    #include <corecrt_malloc.h>
+    #include <corecrt_math.h>
+    #include <corecrt_math_defines.h>
+    #include <corecrt_memcpy_s.h>
+    #include <corecrt_memory.h>
+    #include <corecrt_search.h>
+    #include <corecrt_share.h>
+    #include <corecrt_startup.h>
+    #include <corecrt_stdio_config.h>
+    #include <corecrt_terminate.h>
+    #include <corecrt_wconio.h>
+    #include <corecrt_wctype.h>
+    #include <corecrt_wdirect.h>
+    #include <corecrt_wio.h>
+    #include <corecrt_wprocess.h>
+    #include <corecrt_wstdio.h>
+    #include <corecrt_wstdlib.h>
+    #include <corecrt_wstring.h>
+    #include <corecrt_wtime.h>
+    #include <crtdbg.h>
+    #include <direct.h>
+    #include <dos.h>
+    #include <mbctype.h>
+    #include <io.h>
+    #include <fpieee.h>
+    #include <malloc.h>
+    #include <mbstring.h>
+    #include <minmax.h>
+    #include <new.h>
+    #include <process.h>
+    #include <safeint.h>
+    #include <safeint_internal.h>
+    #include <share.h>
+    #include <tchar.h>
+#endif
+}
+
+#endif  // _$_HX_CORE_M4LIBC
