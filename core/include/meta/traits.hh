a1dc156b813582123a52e0fc10646ebb1d4594b1
Dhruvan <dhruvan.k05@gmail.com>
2025-04-26 15:35:36 -0400

    fix(include): Update header file includes from .h to .hh for consistency in traits module

--- core/include/meta/traits.hh
+++ core/include/meta/traits.hh
@@ -16,7 +16,8 @@
 #ifndef _$_HX_CORE_M6TRAITS
 #define _$_HX_CORE_M6TRAITS
 
-#include <include/config/config.h>
+#include <include/config/config.hh>
+#include <utility>
 
 H_NAMESPACE_BEGIN
 H_STD_NAMESPACE_BEGIN
@@ -46,7 +47,7 @@ constexpr bool same_as<T, T> = true;
 #endif
 
 template <class Up, class T>
-concept convertible_to = is_convertible<Up, T> && requires { static_cast<T>(declval<Up>()); };
+concept convertible_to = is_convertible<Up, T> && requires { static_cast<T>(libcxx::declval<Up>()); };
 
 template <class Up, class T>
 concept is_same_as = same_as<Up, T>;
