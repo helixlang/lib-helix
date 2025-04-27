48de004e7be3ec08ab04441f3e10b98af4f9dac6
Dhruvan <dhruvan.k05@gmail.com>
2025-04-27 10:38:47 -0400

    fix(types): Update CharTraits concept to use is_convertible_to for type checks

--- core/include/types/string/char_traits.hh
+++ core/include/types/string/char_traits.hh
@@ -39,19 +39,19 @@ concept CharTraits = requires(CharT       &dest_char,
     { Traits::eq(src_char, dest_char) } -> std::Meta::is_same_as<bool>;
     { Traits::lt(src_char, dest_char) } -> std::Meta::is_same_as<bool>;
 
-    { Traits::compare(source, target, count) } -> std::Meta::convertible_to<i32>;
-    { Traits::length(source) } -> std::Meta::convertible_to<size_t>;
+    { Traits::compare(source, target, count) } -> std::Meta::is_convertible_to<i32>;
+    { Traits::length(source) } -> std::Meta::is_convertible_to<size_t>;
     { Traits::find(source, count, value) } -> std::Meta::is_same_as<const CharT *>;
 
     { Traits::move(dest_buffer, source, count) } -> std::Meta::is_same_as<CharT *>;
     { Traits::copy(dest_buffer, source, count) } -> std::Meta::is_same_as<CharT *>;
     { Traits::assign(dest_buffer, count, value) } -> std::Meta::is_same_as<CharT *>;
 
-    { Traits::not_eof(code) } -> std::Meta::convertible_to<i32>;
+    { Traits::not_eof(code) } -> std::Meta::is_convertible_to<i32>;
     { Traits::to_char_type(code) } -> std::Meta::is_same_as<CharT>;
-    { Traits::to_int_type(src_char) } -> std::Meta::convertible_to<i32>;
+    { Traits::to_int_type(src_char) } -> std::Meta::is_convertible_to<i32>;
     { Traits::eq_int_type(code, code) } -> std::Meta::is_same_as<bool>;
-    { Traits::eof() } -> std::Meta::convertible_to<i32>;
+    { Traits::eof() } -> std::Meta::is_convertible_to<i32>;
 };
 }  // namespace String
 
